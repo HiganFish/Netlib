a65d7f920bf34f7f81f90457bed19ef5db7696cf
lsmg <rjd67441@hotmail.com>
2020-02-23 01:02:46 -0800

    test: 更改样例为回声服务器

--- project/arkanoid/main.cpp
+++ project/arkanoid/main.cpp
@@ -13,36 +13,37 @@
 
 int pipfd[2];
 
-int fd_array[2];
-int fd_sub = 0;
-
 Net::Reactor *preactor;
 
 void SpliceEach(int sendfd, int recvfd)
 {
     splice(sendfd, nullptr, pipfd[1], nullptr, 32768, SPLICE_F_MORE);
     splice(pipfd[0], nullptr, recvfd, nullptr, 32768, SPLICE_F_MORE);
+//
+//    char* buffer[1024]{};
+//    int ret = recv(sendfd, buffer, sizeof(buffer), 0);
+//    auto* buff = (float*)buffer;
+//
+//    float result = 0;
+//    for (int i = 0; i < ret/ sizeof(float*); ++i)
+//    {
+//        result+=buff[i];
+//    }
+//    printf("%f\n", result);
+//
+//    send(recvfd, buffer, sizeof(buffer), 0);
 }
 
 void read_cb(int fd, int option, int event_type, void *user_data)
 {
-    LOG_INFO("recv a msg");
-    if(fd_sub  == 2)
-    {
-        LOG_INFO("send each a msg");
-        SpliceEach(fd, fd_array[0] == fd ? fd_array[1]:fd_array[0]);
-    }
+    LOG_INFO("send each a msg");
+    SpliceEach(fd, fd);
 }
 
 void listen_cb(int fd, sockaddr_in *addr, size_t addrlen, void *user_data)
 {
     auto *handler = new Net::EventHandler(preactor, fd, EPOLL_CTL_ADD, EPOLLIN | EPOLLRDHUP | EPOLLET, read_cb);
     preactor->AddEventHandler(handler);
-
-    if (fd_sub <= 1)
-    {
-        fd_array[fd_sub++] = fd;
-    }
 }
 
 int main()
@@ -54,7 +55,7 @@ int main()
     Net::Reactor reactor{};
     preactor = &reactor;
 
-    int port = 8000;
+    int port = 8001;
 
     Net::Listener listener{&reactor, listen_cb, port};
 
